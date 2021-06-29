e971a20a39c576742174000771b8456294dafbf7
Gobinath Thangaiya (GOBI) <tgobinath1986@gmail.com>
2021-06-28 22:40:37 -0400

    Mission_to_mars

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-# Set up Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -22,4 +21,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+    app.run(debug=True)
--- scraping.py
+++ scraping.py
@@ -1,34 +1,31 @@
-# Import dependencies 
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-# scrape_all function 
-# - Initialize the browser
-# - Create a data dictionary
-# - End the Web driver and return the scraped data
+
 def scrape_all():
-
     # Initiate headless driver for deployment
-    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
 
-    # Run all scraping functions and store results in dictionary
+    # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "hemisphere_image_info": hemisphere_image(browser)
+        "last_modified": dt.datetime.now()
     }
 
     # Stop webdriver and return data
     browser.quit()
     return data
+
 
 def mars_news(browser):
 
@@ -102,60 +99,5 @@ def mars_facts():
 
 if __name__ == "__main__":
 
-    # If running as script, print scraped data
-    print(scrape_all()
-
-          def hemisphere_image(browser):
-    
-    # Visit the URL 
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
-    
-    # Create a list to hold the images and titles.
-    hemisphere_image_urls = []
-
-    # Code to retrieve the image urls and titles for each hemisphere.
-    # Parse the html with soup
-    html = browser.html
-    main_page_soup = soup(html, 'html.parser')
-
-    # Add try/except for error handling
-    try:
-        # Find the number of pictures to scan
-        pics_count = len(main_page_soup.select("div.item"))
-
-        # for loop over the link of each sample picture
-        for i in range(pics_count):
-            # Create an empty dict to hold the search results
-            results = {}
-            # Find link to picture and open it
-            link_image = main_page_soup.select("div.description a")[i].get('href')
-            browser.visit(f'https://astrogeology.usgs.gov{link_image}')
-        
-            # Parse the new html page with soup
-            html = browser.html
-            sample_image_soup = soup(html, 'html.parser')
-            # Get the full image link
-            img_url = sample_image_soup.select_one("div.downloads ul li a").get('href')
-            # Get the full image title
-            img_title = sample_image_soup.select_one("h2.title").get_text()
-            # Add extracts to the results dict
-            results = {
-                'img_url': img_url,
-                'title': img_title}
-        
-            # Append results dict to hemisphere image urls list
-            hemisphere_image_urls.append(results)
-        
-            # Return to main page
-            browser.back()
-
-    except BaseException:
-        return None
-    
-    # Return the list that holds the dictionary of each image url and title
-    return hemisphere_image_urls    
-
-if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
